6732896f6635e5608f5c6b032507a876d10140f9
nidhal <nidhal.jelassi@fsegt.utm.tn>
2022-05-16 14:25:50 +0100

    project

--- src/app/cv/cv.component.ts
+++ src/app/cv/cv.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Candidat } from '../models/candidat';
 
 @Component({
   selector: 'app-cv',
   templateUrl: './cv.component.html',
-  styleUrls: ['./cv.component.css']
+  styleUrls: ['./cv.component.css'],
 })
 export class CvComponent implements OnInit {
+  tabCandidats = [
+    new Candidat(1, 'Nidhal', 'Jelassi', 38, 'Enseignant', 'nidhal.jpg'),
+    new Candidat(2, 'Bart', 'Simpson', 18, 'Eleve', 'bart.jpeg'),
+    new Candidat(3, 'Homer', 'Simpson', 52, 'Directeur', 'homer.jpg'),
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
---
+++ src/app/models/candidat.ts
@@ -0,0 +1,10 @@
+export class Candidat {
+  constructor(
+    public _id: number,
+    public prenom: string,
+    public nom: string,
+    public age: number,
+    public profession: string,
+    public avatar: string
+  ) {}
+}
