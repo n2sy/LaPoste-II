0f3a32acb86626d26fea8800947f51e8cd53b338
nidhal <nidhal.jelassi@fsegt.utm.tn>
2022-05-17 13:13:06 +0100

    add ngclass

--- src/app/direct/direct.component.ts
+++ src/app/direct/direct.component.ts
@@ -8,7 +8,23 @@ import { Component, OnInit } from '@angular/core';
 export class DirectComponent implements OnInit {
   bg = 'pink';
   cl = 'grey';
+  class1 = true;
+  class2 = false;
+  class3 = false;
   constructor() {}
 
   ngOnInit(): void {}
+
+  switchClass() {
+    if (this.class1) {
+      this.class1 = false;
+      this.class2 = true;
+    } else if (this.class2) {
+      this.class2 = false;
+      this.class3 = true;
+    } else {
+      this.class3 = false;
+      this.class1 = true;
+    }
+  }
 }
